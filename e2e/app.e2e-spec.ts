5db28d7212225f6466cfcbafd62874f4ee50a520
David <david.norton@cgi.com>
2018-02-06 11:50:09 +0000

    Fixed e2e test

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -7,8 +7,8 @@ describe('portfolio App', () => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
+  it('should display name in navbar', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+    expect(page.getParagraphText()).toEqual('David Norton');
   });
 });
