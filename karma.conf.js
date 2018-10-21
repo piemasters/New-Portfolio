08c1ea8f5acbf116a601dc02214b82f2f3ae4bdc
David Norton <pie_masters@hotmail.co.uk>
2018-10-21 12:11:55 +0100

    Updated Karma config to use headless chrome

--- karma.conf.js
+++ karma.conf.js
@@ -27,10 +27,10 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: ['Chrome', 'ChromeHeadless'],
     customLaunchers: {
-      ChromeNoSandbox: {
-        base: 'Chrome',
+      ChromeHeadlessNoSandbox: {
+        base: 'ChromeHeadless',
         flags: ['--no-sandbox']
       }
     },
