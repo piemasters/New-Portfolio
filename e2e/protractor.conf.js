461dac54422e641a72a792ef51bd7003a54baed6
David Norton <pie_masters@hotmail.co.uk>
2018-10-25 00:14:56 +0100

    Fixed e2e cucumber testing with Angular 7

---
+++ e2e/protractor.conf.js
@@ -0,0 +1,34 @@
+// Protractor configuration file, see link for more information
+// https://github.com/angular/protractor/blob/master/lib/config.ts
+exports.config = {
+  allScriptsTimeout: 11000,
+  baseUrl: 'http://localhost:4200/',
+  capabilities: {
+    'browserName': 'chrome',
+    chromeOptions: {
+      args: ['--no-sandbox']
+    }
+  },
+  directConnect: true,
+  specs: [
+    './features/**/*.feature'
+  ],
+  framework: 'custom',
+  frameworkPath: require.resolve('protractor-cucumber-framework'),
+  cucumberOpts: {
+    require: [
+      './steps/**/*.ts'
+    ],
+    strict: true,
+    format: [
+      'json:e2e/report.json'
+    ],
+    dryRun: false,
+    compiler: []
+  },
+  onPrepare() {
+    require('ts-node').register({
+      project: require('path').join(__dirname, './tsconfig.e2e.json')
+    });
+  }
+};
--- e2e/steps/navigation.steps.ts
+++ e2e/steps/navigation.steps.ts
@@ -1,24 +1,30 @@
 import { expect } from 'chai';
-import { defineSupportCode, setDefaultTimeout } from 'cucumber';
+import { Before, Given, setDefaultTimeout, Then, When } from 'cucumber';
 import { AppPage } from './navigation.po';
 
 setDefaultTimeout(60 * 1000);
 
-defineSupportCode(({Given, When, Then, Before}) => {
-  let app: AppPage;
+let app: AppPage;
 
-  Before(() => {
-    app = new AppPage();
-  });
-
-  Given('I am on the portfolio homepage',
-    () => app.navigateTo());
-
-  When('I navigate to the {string} page',
-    (page: string) => app.navigateToPage(page));
-
-  Then('I should see the {string} title',
-    (title: string) => app.getPageHeader()
-      .then(page_header => expect(page_header).to.equal(title)));
-
+Before(() => {
+  app = new AppPage();
 });
+
+Given('I am on the portfolio homepage',
+  () => app.navigateTo());
+
+When('I navigate to the {string} page',
+  (page: string) => app.navigateToPage(page));
+
+// Then('I should see the {string} title',
+//   (title: string) => app.getPageHeader()
+//   .then(page_header => expect(page_header).to.equal(title)));
+
+Then('I should see the {string} title', (title: string) => {
+  app.getPageHeader().then(page_header => expect(page_header).to.equal(title))
+});
+  //expect(app.getPageHeader().getText()).toEqual(title);
+
+
+
+
