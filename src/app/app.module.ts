cb5aad03c356cda75a2f299f2625f73b18075ded
David <david.norton@cgi.com>
2018-12-21 16:15:04 +0000

    Started adding forefront dependencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,6 +9,13 @@ import { ProjectsModule } from './entities/projects/projects.module';
 import { CustomPipesModule } from './shared/pipes/custom-pipes.module';
 import { NavbarComponent } from './core/layouts/navbar/navbar.component';
 import { FooterComponent } from './core/layouts/footer/footer.component';
+import { StoreModule } from '@ngrx/store';
+import { environment } from '../environments/environment';
+import { StoreRouterConnectingModule } from '@ngrx/router-store';
+import { EffectsModule } from '@ngrx/effects';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+
 
 @NgModule({
   declarations: [
@@ -16,11 +24,18 @@ import { FooterComponent } from './core/layouts/footer/footer.component';
     FooterComponent
   ],
   imports: [
-    BrowserModule,
+    BrowserModule.withServerTransition({appId: 'portfolio'}),
+    BrowserAnimationsModule,
     HttpClientModule,
     AppRoutingModule,
     ProjectsModule,
     CustomPipesModule,
+    StoreModule.forRoot(reducers, { metaReducers }),
+    !environment.production ? StoreDevtoolsModule.instrument()  : [],
+    StoreRouterConnectingModule.forRoot({stateKey: 'router'}),
+    EffectsModule.forRoot([ AuthEffects ]),
+    StoreRouterConnectingModule,
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
   bootstrap: [AppComponent]
 })
