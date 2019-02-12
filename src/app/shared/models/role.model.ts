54a2be78faa1aa53656a9cefc8bd9f9e3ca4b033
David <david.norton@cgi.com>
2019-02-12 17:29:40 +0000

    Added content to resume page

--- src/app/shared/models/role.model.ts
+++ src/app/shared/models/role.model.ts
@@ -1,12 +1,12 @@
 export class Role {
   public id: number;
   public headline: string;
-  public description: string;
+  public description: string[];
   public from: number;
   public to: number;
   public location: string;
 
-  constructor(id: number, headline: string, description: string, from: number, to: number, location: string) {
+  constructor(id: number, headline: string, description: string[], from: number, to: number, location: string) {
     this.id = id;
     this.headline = headline;
     this.description = description;
