459711a273b6532e89fe5a4d077d3d4d808794c5
David Norton <pie_masters@hotmail.co.uk>
2019-03-02 19:24:28 +0000

    Added text for competitive analysis & user/stakeholder interviews Improved process type styling

--- src/app/shared/models/ux-method.model.ts
+++ src/app/shared/models/ux-method.model.ts
@@ -1,14 +1,16 @@
 export class UXMethod {
   public id: string;
   public name: string;
+  public step: string;
   public type: string;
   public properties: any;
   public links: string[];
   public description: any[];
 
-  constructor(id: string, name: string, type: string, properties: any, links: string[], description: any[]) {
+  constructor(id: string, name: string, step: string, type: string, properties: any, links: string[], description: any[]) {
     this.id = id;
     this.name = name;
+    this.step = step;
     this.type = type;
     this.properties = properties;
     this.links = links;
